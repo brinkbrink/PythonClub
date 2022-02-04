aff21da4536859bf4ea767095e9e8a646437017f
brinkbrink <brinkbrink@gmail.com>
2022-02-04 12:16:58 -0800

    a4

--- club/admin.py
+++ club/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Meeting, MeetingMin, Resource, Event
 # Register your models here.
+admin.site.register(Meeting)
+admin.site.register(MeetingMin)
+admin.site.register(Resource)
+admin.site.register(Event)
--- club/models.py
+++ club/models.py
@@ -1,3 +1,61 @@
 from django.db import models
+from django.forms import DateField
+from django.contrib.auth.models import User
 
 # Create your models here.
+# Meeting which will have fields for meeting title, meeting date, meeting time, location, Agenda
+# Meeting Minutes which will have fields for meeting id (a foreign key), attendance (a many to many field with User), Minutes text
+# Resource which will have fields for resource name, resource type, URL, date entered, user id (foreign key with User), and description
+# Event which will have fields for event title, location, date, time, description and the user id of the member that posted it
+
+class Meeting(models.Model):
+    title=models.CharField(max_length=250)
+    date=models.DateField()
+    time=models.TimeField()
+    location=models.CharField(max_length=250)
+    agenda=models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        db_table='meeting'
+
+class MeetingMin(models.Model):
+    meetid=models.ForeignKey(Meeting, on_delete=models.CASCADE)
+    userid=models.ForeignKey(User, on_delete=models.CASCADE)
+    minutes=models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.meetid
+
+    class Meta:
+        db_table='meetingmin'
+
+class Resource(models.Model):
+    name=models.CharField(max_length=250)
+    type=models.CharField(max_length=250)
+    url=models.URLField()
+    dateentered=DateField()
+    userid=models.ForeignKey(User, on_delete=models.CASCADE)
+    description=models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table='resource'
+
+class Event(models.Model):
+    title=models.CharField(max_length=250)
+    location=models.CharField(max_length=250)
+    date=models.DateField()
+    time=models.TimeField()
+    evdescription=models.TextField(null=True, blank=True)
+    userid=models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        db_table='event'
